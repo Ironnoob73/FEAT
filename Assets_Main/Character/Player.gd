e6b4517e1b32efdb74f5b77528e267f618cb4a2e
低耐久的铁砧 <67966070+Ironnoob73@users.noreply.github.com>
2024-03-17 16:09:36 +0800

    20240317DU1

--- Assets_Main/Character/Player.gd
+++ Assets_Main/Character/Player.gd
@@ -93,6 +93,71 @@ func _unhandled_input(_event):
 			current_hotbar = 4
 			refresh_handheld(current_hotbar)
 			
+
+#from : https://github.com/majikayogames/godot-character-controller-stairs/blob/main/entities/Player/Player.gd
+var _was_on_floor_last_frame = false
+var _snapped_to_stairs_last_frame = false
+func _snap_down_to_stairs_check():
+	var did_snap = false
+	if not is_on_floor() and velocity.y <= 0 and (_was_on_floor_last_frame or _snapped_to_stairs_last_frame) and $StairsBelowRayCast3D.is_colliding():
+		var body_test_result = PhysicsTestMotionResult3D.new()
+		var params = PhysicsTestMotionParameters3D.new()
+		var max_step_down = -0.5
+		params.from = self.global_transform
+		params.motion = Vector3(0,max_step_down,0)
+		if PhysicsServer3D.body_test_motion(self.get_rid(), params, body_test_result):
+			var translate_y = body_test_result.get_travel().y
+			self.position.y += translate_y
+			apply_floor_snap()
+			did_snap = true
+
+	_was_on_floor_last_frame = is_on_floor()
+	_snapped_to_stairs_last_frame = did_snap
+	
+@onready var _initial_separation_ray_dist = abs($StepUpSeparationRay_F.position.z)
+var _last_xz_vel : Vector3 = Vector3(0,0,0)
+func _rotate_step_up_separation_ray():
+	var xz_vel = velocity * Vector3(1,0,1)
+	
+	if xz_vel.length() < 0.1:
+		xz_vel = _last_xz_vel
+	else:
+		_last_xz_vel = xz_vel
+		
+	var xz_f_ray_pos = xz_vel.normalized() * _initial_separation_ray_dist
+	$StepUpSeparationRay_F.global_position.x = self.global_position.x + xz_f_ray_pos.x
+	$StepUpSeparationRay_F.global_position.z = self.global_position.z + xz_f_ray_pos.z
+
+	var xz_l_ray_pos = xz_f_ray_pos.rotated(Vector3(0,1.0,0), deg_to_rad(-50))
+	$StepUpSeparationRay_L.global_position.x = self.global_position.x + xz_l_ray_pos.x
+	$StepUpSeparationRay_L.global_position.z = self.global_position.z + xz_l_ray_pos.z
+	
+	var xz_r_ray_pos = xz_f_ray_pos.rotated(Vector3(0,1.0,0), deg_to_rad(50))
+	$StepUpSeparationRay_R.global_position.x = self.global_position.x + xz_r_ray_pos.x
+	$StepUpSeparationRay_R.global_position.z = self.global_position.z + xz_r_ray_pos.z
+	
+	# To prevent character from running up walls, we do a check for how steep
+	# the slope in contact with our separation rays is
+	$StepUpSeparationRay_F/RayCast3D.force_raycast_update()
+	$StepUpSeparationRay_L/RayCast3D.force_raycast_update()
+	$StepUpSeparationRay_R/RayCast3D.force_raycast_update()
+	var max_slope_ang_dot = Vector3(0,1,0).rotated(Vector3(1.0,0,0), self.floor_max_angle).dot(Vector3(0,1,0))
+	var any_too_steep = false
+	if $StepUpSeparationRay_F/RayCast3D.is_colliding() and $StepUpSeparationRay_F/RayCast3D.get_collision_normal().dot(Vector3(0,1,0)) < max_slope_ang_dot:
+		any_too_steep = true
+	if $StepUpSeparationRay_L/RayCast3D.is_colliding() and $StepUpSeparationRay_L/RayCast3D.get_collision_normal().dot(Vector3(0,1,0)) < max_slope_ang_dot:
+		any_too_steep = true
+	if $StepUpSeparationRay_R/RayCast3D.is_colliding() and $StepUpSeparationRay_R/RayCast3D.get_collision_normal().dot(Vector3(0,1,0)) < max_slope_ang_dot:
+		any_too_steep = true
+	
+	$StepUpSeparationRay_F.disabled = any_too_steep
+	$StepUpSeparationRay_L.disabled = any_too_steep
+	$StepUpSeparationRay_R.disabled = any_too_steep
+	
+var _cur_frame = 0
+@export var _jump_frame_grace = 5
+var _last_frame_was_on_floor = -_jump_frame_grace - 1
+			
 func _physics_process(_delta):
 	# Record Inerita & Add the gravity.
 	if is_on_floor():
@@ -102,7 +167,10 @@ func _physics_process(_delta):
 		velocity.y -= gravity * 0.05
 
 	# Handle Jump.
-	if Input.is_action_pressed("ui_accept") and is_on_floor():
+	_cur_frame += 1
+	if is_on_floor():
+		_last_frame_was_on_floor = _cur_frame
+	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or _cur_frame - _last_frame_was_on_floor <= _jump_frame_grace):
 		velocity.y = JUMP_VELOCITY
 	
 	# Move Input.
@@ -135,7 +203,9 @@ func _physics_process(_delta):
 		player_collision.shape.height = lerp(player_collision.shape.height,1.8,0.5)
 		player_camera.position.y = lerp(player_camera.position.y,0.5,0.5)
 	
+	_rotate_step_up_separation_ray()
 	move_and_slide()
+	_snap_down_to_stairs_check()
 
 	#Scroll hotbar
 	if current_menu == "HUD" and !Input.is_action_pressed("tool_function_switch"):
