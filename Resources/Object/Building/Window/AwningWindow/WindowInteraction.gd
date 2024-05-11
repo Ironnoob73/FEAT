cc45cf3e94125fae663a9557c8abac0886eb3ad8
低耐久的铁砧 <67966070+Ironnoob73@users.noreply.github.com>
2024-05-11 14:09:40 +0800

    20240511DU2

---
+++ Resources/Object/Building/Window/AwningWindow/WindowInteraction.gd
@@ -0,0 +1,9 @@
+extends AnimatableBody3D
+
+var open : bool = false
+
+func interact():
+	var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART)
+	open = !open
+	if open :	tween.tween_property(self, "rotation:x", deg_to_rad(60), 0.5)
+	else :		tween.tween_property(self, "rotation:x", 0, 0.5)
