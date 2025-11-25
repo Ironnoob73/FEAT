bf92b878707a5fde92e494490d4c77758c7914c1
Ironnoob73 <67966070+Ironnoob73@users.noreply.github.com>
2025-11-25 13:04:26 +0800

    Window function

--- Resources/Object/Device/Computer/Sreen/computer_screen.gd
+++ Resources/Object/Device/Computer/Sreen/computer_screen.gd
@@ -4,6 +4,23 @@ extends TextureRect
 
 @onready var files_icon: Button = $DesktopIcons/FilesIcon
 
+@onready var window_class = preload("res://Resources/Object/Device/Computer/Sreen/window.tscn")
+@onready var window_frame_anim: NinePatchRect = $WindowFrameAnim
+
 func _process(_delta: float) -> void:
 	var time_dict : Dictionary = Time.get_time_dict_from_system()
 	time_label.text = str("%02d" % time_dict.get("hour"), ":", "%02d" % time_dict.get("minute"), " ")
+
+func _on_files_icon_pressed() -> void:
+	move_child(window_frame_anim,-1)
+	window_frame_anim.size = files_icon.size
+	window_frame_anim.position = files_icon.position
+	window_frame_anim.show()
+	var window = window_class.instantiate()
+	window.hide()
+	add_child(window)
+	var tween = create_tween()
+	tween.tween_property(window_frame_anim,"size",window.size,0.25)
+	tween.set_parallel().tween_property(window_frame_anim,"position",window.position,0.25)
+	tween.set_parallel(false).tween_callback(func():window_frame_anim.hide())
+	tween.tween_callback(func():window.show())
---
+++ Resources/Object/Device/Computer/Sreen/window.gd
@@ -0,0 +1,28 @@
+extends Panel
+
+## @tutorial(From): https://github.com/popcar2/GodotOS/blob/main/Scenes/Window/window.gd
+
+var is_dragging: bool
+var start_drag_position: Vector2
+var mouse_start_drag_position: Vector2
+
+func _process(_delta: float) -> void:
+	if is_dragging:
+		global_position = start_drag_position + (get_global_mouse_position() - mouse_start_drag_position)
+		#clamp_window_inside_viewport()
+
+func _on_title_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == 1:
+		if event.is_pressed():
+			on_focus()
+			is_dragging = true
+			start_drag_position = global_position
+			mouse_start_drag_position = get_global_mouse_position()
+		else:
+			is_dragging = false
+
+func _on_close_button_pressed() -> void:
+	queue_free()
+
+func on_focus():
+	get_parent().move_child(self,-1)
